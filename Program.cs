05efa3b54150a47791a56c0d134aad16fd7adca7
Sefa <sefayesilarslan1922@gmail.com>
2023-08-07 14:18:54 +0300

    denemeler

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Degiskenler
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+        string yazi="deneme";
+        int a=30;
+        int b=20;
+        int toplam=a+b;
+        Console.WriteLine(toplam);
+
+        string dateTime= DateTime.Now.ToString("dd.MM.yyyy");
+        Console.WriteLine(dateTime);
+        }
+
+    }
+}
